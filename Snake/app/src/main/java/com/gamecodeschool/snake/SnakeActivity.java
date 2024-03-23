32ee3678868f87d01b9d48681db43faaa2a12484
Bejan Maljai <bmaljai@gmail.com>
2024-03-22 19:08:58 -0700

    added SnakeActivity code

--- Snake/app/src/main/java/com/gamecodeschool/snake/SnakeActivity.java
+++ Snake/app/src/main/java/com/gamecodeschool/snake/SnakeActivity.java
@@ -10,12 +10,40 @@ import android.view.Display;
 import android.view.Window;
 
 public class SnakeActivity extends Activity {
+    // Declare an instance of SnakeGame
     SnakeGame mSnakeGame;
-
-
+    // Set the game up
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+// Get the pixel dimensions of the screen
+        Display display = getWindowManager()
+                .getDefaultDisplay();
+// Initialize the result into a Point object
+        Point size = new Point();
+        display.getSize(size);
+// Create a new instance of the SnakeGame class
+        mSnakeGame = new SnakeGame(this, size);
+// Make snakeGame the view of the Activity
+        setContentView(mSnakeGame);
     }
+    // Start the thread in snakeGame
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSnakeGame.resume();
+    }
+    // Stop the thread in snakeGame
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSnakeGame.pause();
+    }
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//    }
 }
