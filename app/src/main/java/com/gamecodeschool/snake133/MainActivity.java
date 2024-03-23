3182426ad0a1c4c3453621633247603886184181
TigEash <shade.death.shade+1@gmail.com>
2024-03-23 01:58:18 -0700

    Added some temp drawing code
    Added some temp drawing code to the main activity file to check that it's doing something

--- app/src/main/java/com/gamecodeschool/snake133/MainActivity.java
+++ app/src/main/java/com/gamecodeschool/snake133/MainActivity.java
@@ -1,14 +1,23 @@
 package com.gamecodeschool.snake133;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
+    DrawView drawView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+      //Some place holder code to make some basic draw calls
+        drawView = new DrawView(this);
+        drawView.setBackgroundColor(Color.BLACK);
+        setContentView(drawView);
+
     }
+
 }
