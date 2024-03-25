96df86d51ff834d3dc0a3f5a85ceeabd6c26a7c0
Bejan Maljai <bmaljai@gmail.com>
2024-03-24 19:32:18 -0700

    added new texture background, but still bugs, added author names to top right

--- Snake/app/src/main/java/com/gamecodeschool/snake/SnakeActivity.java
+++ Snake/app/src/main/java/com/gamecodeschool/snake/SnakeActivity.java
@@ -1,49 +1,45 @@
 package com.gamecodeschool.snake;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
 import android.app.Activity;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.view.Display;
-import android.view.Window;
 
 public class SnakeActivity extends Activity {
+
     // Declare an instance of SnakeGame
     SnakeGame mSnakeGame;
+
     // Set the game up
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-// Get the pixel dimensions of the screen
-        Display display = getWindowManager()
-                .getDefaultDisplay();
-// Initialize the result into a Point object
+
+        // Get the pixel dimensions of the screen
+        Display display = getWindowManager().getDefaultDisplay();
+
+        // Initialize the result into a Point object
         Point size = new Point();
         display.getSize(size);
-// Create a new instance of the SnakeGame class
+
+        // Create a new instance of the SnakeEngine class
         mSnakeGame = new SnakeGame(this, size);
-// Make snakeGame the view of the Activity
+
+        // Make snakeEngine the view of the Activity
         setContentView(mSnakeGame);
     }
-    // Start the thread in snakeGame
+
+    // Start the thread in snakeEngine
     @Override
     protected void onResume() {
         super.onResume();
         mSnakeGame.resume();
     }
-    // Stop the thread in snakeGame
+
+    // Stop the thread in snakeEngine
     @Override
     protected void onPause() {
         super.onPause();
         mSnakeGame.pause();
     }
-
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//    }
 }
